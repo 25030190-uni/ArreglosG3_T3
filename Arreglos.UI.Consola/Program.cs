766bb0e1342d0e0d9897f136ea9a2c607edc5678
25030190-uni <25030190@itesa.edu.mx>
2026-09-17 07:27:55 -0600

    Vercion_02: Metodo Llenar Arreglo.

--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Arreglos.Logica;
+
+Console.WriteLine("Hello, World!");
+
+
+
+MiArreglo oMiArreglo = new MiArreglo(1000);
+
+oMiArreglo.Llenar(5,50);
+Console.WriteLine(oMiArreglo); // llama automáticamente a ToString()
